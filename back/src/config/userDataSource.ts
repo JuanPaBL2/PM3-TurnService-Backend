3cb1e7257ebc8077b16544e693c6bb68e5b5e7e1
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-07-21 19:30:04 -0300

    fix

--- back/src/config/userDataSource.ts
+++ back/src/config/userDataSource.ts
@@ -7,7 +7,12 @@ require("dotenv").config();
 
 
 export const AppDataSource = new DataSource({ 
-    type: 'postgres', 
+    type: 'postgres',  // Asumiendo que estás usando PostgreSQL
+    host: process.env.PGHOST, // O puedes usar DATABASE_URL si contiene toda la información
+    port: parseInt(process.env.PGPORT || '5432', 10), // PostgreSQL usa el puerto 5432 por defecto
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE || process.env.POSTGRES_DB, // Usar POSTGRES_DB si PGDATABASE no está disponible
     url: process.env.DATABASE_URL,
     synchronize: true,
     logging: true, 
